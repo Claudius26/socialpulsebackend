50150626eb173e40427a22e558b9877c0a6b44a7
Claudius26 <osomzed@gmail.com>
2025-10-25 05:58:28 +0100

    added cores

--- socialpulse/settings.py
+++ socialpulse/settings.py
@@ -87,7 +87,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://socialpulse-cstr.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (

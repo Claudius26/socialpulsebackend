a31a88217b736bd55e3ab6e846f49e53ec5a710a
Claudius26 <williamskou396@gmail.com>
2026-06-30 02:28:26 -0700

    Admin login: accept username OR email; drop debug print

--- users/serializers_admin.py
+++ users/serializers_admin.py
@@ -1,21 +1,23 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
 class AdminLoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
+    username = serializers.CharField()  # accepts username OR email
     password = serializers.CharField(write_only=True)
 
     def validate(self, attrs):
-        username = attrs.get("username")
+        ident = (attrs.get("username") or "").strip()
         password = attrs.get("password")
 
-        user = User.objects.filter(username=username).first()
+        user = User.objects.filter(
+            Q(username__iexact=ident) | Q(email__iexact=ident)
+        ).first()
         if not user or not user.check_password(password):
             raise serializers.ValidationError("Invalid credentials")
-        print(user.is_staff)
 
         if not user.is_staff:
             raise serializers.ValidationError("Not authorized as admin")
